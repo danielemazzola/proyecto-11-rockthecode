16b4086592af697f22f05144dbe2cf1f0ac1998d
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-07-05 02:03:28 +0200

    FIX position browser router and my hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Home from './pages/home/Home'
 import Biography from './pages/biography/Biography'
 import NotFound from './pages/not-found/NotFound'
-import { CharsProvider } from './context/Context'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <CharsProvider>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index='/home' element={<Home />} />
-            <Route path='/biography/:character' element={<Biography />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
-        </Routes>
-      </CharsProvider>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index='/home' element={<Home />} />
+        <Route path='/biography/:character' element={<Biography />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { CharsProvider } from './context/Context.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <CharsProvider>
+      <App />
+    </CharsProvider>
+  </BrowserRouter>
+)
