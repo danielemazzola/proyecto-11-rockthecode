125b25185d2650fd894f9498894dc0ef0d109096
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-07-09 16:34:08 +0200

    component ToggleTheme

--- src/components/toggleTheme/ToggleTheme.jsx
+++ src/components/toggleTheme/ToggleTheme.jsx
@@ -3,14 +3,17 @@ import { ThemeContext } from '../../context/ThemeContext'
 import './ToggleTheme.css' // Asegúrate de importar el archivo CSS
 
 const ToggleTheme = () => {
-  const { state, toggleTheme } = useContext(ThemeContext)
+  const {
+    state: { theme },
+    toggleTheme
+  } = useContext(ThemeContext)
 
   return (
     <button
       type='button'
       onClick={toggleTheme}
       className={`toggle-button ${
-        state.theme === 'light' ? 'light-theme' : 'dark-theme'
+        theme === 'light' ? 'light-theme' : 'dark-theme'
       }`}
     >
       Toggle Theme
